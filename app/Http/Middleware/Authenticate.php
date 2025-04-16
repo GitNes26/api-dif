1bb9b30b7ad2d1b37b4337dfae95617e296524dc
Nestor <nestorpuentesin@gmail.com>
2025-04-16 10:08:03 -0600

    se agrego condicion al middleware Auth para regresar un 401

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class Authenticate extends Middleware
 {
@@ -12,6 +13,11 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        if (!$request->expectsJson()) {
+            abort(response()->json([
+                'message' => 'No autenticado.',
+            ], 401));
+        }
+        // return $request->expectsJson() ? null : route('login');
     }
 }
